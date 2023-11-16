8423a485f4d3613d51d71bbe91245ab813697390
outsourcing_billing1 <outsourcing_billing1@ecloudvally.com>
2023-11-16 06:15:03 +0000

    perf(): monitor perfomence

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 // import { NestExpressApplication } from '@nestjs/platform-express';
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
+
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 import { AppModule } from './app.module';
 
@@ -11,12 +16,32 @@ function MiddleWareToAll(res: any, req: any, next: any) {
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
-        logger: ['warn'],
+        logger: WinstonModule.createLogger({
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        nestWinstonModuleUtilities.format.nestLike(),
+                    ),
+                }),
+                // 其他 transports...
+            ],
+        }),
+    });
+
+    Sentry.init({
+        dsn: 'https://b49504f68a362512886eed70618d4885@o4506058875928576.ingest.sentry.io/4506064666820609',
+        integrations: [new ProfilingIntegration()],
+        // Performance Monitoring
+        tracesSampleRate: 1.0,
+        // Set sampling rate for profiling - this is relative to tracesSampleRate
+        profilesSampleRate: 1.0,
     });
     // app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static/' }); // static folder
     // app.setBaseViewsDir(join(__dirname, '..', 'views')); // views folder
     // app.setViewEngine('hbs'); // view engine
     // app.setGlobalPrefix('api/v1.0'); // global prefix
+
     app.use(MiddleWareToAll); // global middleware
     app.enableCors(); // cors
     // useContainer(app.select(AppModule), { fallbackOnErrors: true });
